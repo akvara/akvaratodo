d776ce088814d7bc4834a52d4208be92682b9fe2
Andrius K <akvara@gmail.com>
2018-01-28 13:10:26 +0200

    GMT for Safari

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,8 +2,8 @@ import _ from 'underscore';
 import CONFIG from '../config.js';
 
 
-exports.sortArrOfObjectsByParam = function(arrToSort, sortBy, sortDesc) {
-    if(!sortDesc) {
+exports.sortArrOfObjectsByParam = function (arrToSort, sortBy, sortDesc) {
+    if (!sortDesc) {
         return arrToSort.sort(function (a, b) {
             return a[sortBy] < b[sortBy] ? -1 : 1;
         });
@@ -13,9 +13,9 @@ exports.sortArrOfObjectsByParam = function(arrToSort, sortBy, sortDesc) {
             return a[sortBy] > b[sortBy] ? -1 : 1;
         });
     }
-}
+};
 
-exports.moveToAnother = function(fromA, toB, i, toTop) {
+exports.moveToAnother = function (fromA, toB, i, toTop) {
     let trans = fromA[i];
     fromA.splice(i, 1);
     if (toTop) {
@@ -25,54 +25,54 @@ exports.moveToAnother = function(fromA, toB, i, toTop) {
     }
 
     return {A: fromA, B: toB};
-}
+};
 
-exports.moveToEnd = function(items, i) {
+exports.moveToEnd = function (items, i) {
     let trans = items[i];
     items.splice(i, 1);
 
     return items.concat([trans]);
-}
+};
 
-exports.moveToTop = function(items, i) {
+exports.moveToTop = function (items, i) {
     let trans = items[i];
     items.splice(i, 1);
 
     return [trans].concat(items);
-}
+};
 
-exports.moveFromTo = function(items, fromPos, toPos) {
+exports.moveFromTo = function (items, fromPos, toPos) {
     let trans = items[fromPos];
     items.splice(fromPos, 1);
     items.splice(toPos, 0, trans);
 
     return items;
-}
+};
 
-exports.removeItem = function(items, i) {
+exports.removeItem = function (items, i) {
     items.splice(i, 1);
 
     return items;
-}
+};
 
-exports.textToArray = function(text) {
+exports.textToArray = function (text) {
     return text.split(/\r?\n/).filter(entry => entry.trim() !== '');
-}
+};
 
-exports.overLength = function(which, items) {
+exports.overLength = function (which, items) {
     return items.length > CONFIG.user.settings[which];
-}
+};
 
-exports.grabDate = function(someDateStr) {
+exports.grabDate = function (someDateStr) {
     return toLocalTime(someDateStr).substr(0, 10);
-}
+};
 
-exports.grabTime = function(someDateStr) {
+exports.grabTime = function (someDateStr) {
     return toLocalTime(someDateStr).substr(11, 5);
-}
+};
 
 function toLocalTime(utcDateStr) {
-    return new Date(new Date(utcDateStr).toString() + " UTC").toISOString();
+    return new Date(new Date(utcDateStr).toString().replace(/GMT.*/g, "") + " UTC").toISOString();
 }
 
 exports.toLocalTime = toLocalTime;
